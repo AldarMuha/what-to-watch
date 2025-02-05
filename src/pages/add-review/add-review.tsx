b1db112271c1508ea1d3cf18879582628ef845b9
AidarMuhametzianov <aldarmuha1992@gmail.com>
2025-02-05 21:01:03 +0500

    add-review добавил изменения

--- src/pages/add-review/add-review.tsx
+++ src/pages/add-review/add-review.tsx
@@ -12,13 +12,16 @@ function AddReview(): JSX.Element {
   if (!film) {
     navigate(AppRoute.Login);
   }
+  const styleColor = {
+    backgroundColor: film?.backgroundColor,
+  };
   return (
-    <section className="film-card film-card--full">
+    <section className="film-card film-card--full" style={styleColor}>
       <div className="film-card__header">
         <div className="film-card__bg">
           <img
-            src="img/bg-the-grand-budapest-hotel.jpg"
-            alt="The Grand Budapest Hotel"
+            src={film?.backgroundImage}
+            alt={film?.name}
           />
         </div>
         <h1 className="visually-hidden">WTW</h1>
@@ -40,8 +43,8 @@ function AddReview(): JSX.Element {
         </header>
         <div className="film-card__poster film-card__poster--small">
           <img
-            src="img/the-grand-budapest-hotel-poster.jpg"
-            alt="The Grand Budapest Hotel poster"
+            src={film?.posterImage}
+            alt={film?.name}
             width={218}
             height={327}
           />
